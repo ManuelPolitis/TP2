71bfb67a4d717d618db3069a17ee054a106d6c17
Manuel <mpolitis@fi.uba.ar>
2022-06-16 19:12:06 -0300

    comento y agrego pre y postcondiciones

--- youtube_api_script.py
+++ youtube_api_script.py
@@ -6,6 +6,9 @@ import pickle
 import json
 
 def apertura():
+    """Precondiciones: Apertura hara el request de las tokens a la api, generando una autentificacion, pero antes verificara si ya existe una que no este expirada. En el caso que este expirada usara la refresh token. Con la token
+    obtendremos los permisos para acceder a los datos que necesitamos del usuario a traves de la api.
+    Postcondiciones: La funcion apertura retornara las credenciales que nos entrego la api para poder empezara utilizar sus funciones"""
 
     credentials = None
 
@@ -41,6 +44,7 @@ def apertura():
             with open('token.pickle', 'wb') as f:
                 print('Guardando las credenciales para proximo uso...')
                 pickle.dump(credentials, f)
+    print(type(credentials))
 
     return credentials
 
@@ -48,25 +52,27 @@ def apertura():
 ###################################
 
 
-def nombre_playlists():
+def nombre_playlists()->None:
+    """Precondiciones: Nombre playlist hara un request a la api sobre los datos de las playlist de los usuarios. Luego mediante bucles o recorrido del diccionario que llega de la response
+    obtendremos los datos del nombre del canal y las playlists"""
+
 
     credentials = apertura()
 
     youtube = build('youtube', 'v3', credentials=credentials)
 
-    request = youtube.playlists().list(part="snippet",mine=True)
+    request = youtube.playlists().list(part="snippet",mine=True) #Genero el request q con el part=snippet me dara toda la informacion que necesito. mine=true hara que busque en el canal de la persona que se autentico
     response = request.execute()
 
-    cant_de_playlists:int = len(response["items"])
+    cant_de_playlists:int = len(response["items"]) # Obtengo la cantidad de playlists tomando cuantos diccionarios hay dentro de items, que son las playlists
 
     nombre_de_playlists:list=[]
 
-    nombre_de_canal:str = response["items"][0]["snippet"]["channelTitle"]
+    nombre_de_canal:str = response["items"][0]["snippet"]["channelTitle"] # Dentro del diccionario response, obtengo le nombre del canal recorriendo por sus keys
 
-    for i in range (0,cant_de_playlists):
+    for i in range (0,cant_de_playlists): #Recorro todas las playlists obtenidas como diccionarios para sacar solamente los titulos de las mismas
         nombre_de_playlists.append((response["items"][i]["snippet"]["title"]))
 
     print("Plataforma: Youtube")
     print(f"Usuario: {nombre_de_canal}")
     print(f"El nombre de las playlist es: {nombre_de_playlists}")
-
