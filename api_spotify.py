8f332973618fe971aeed0070217363080a611035
Infernalizado <apanozzo@fi.uba.ar>
2022-06-19 01:30:55 -0300

    Agregada funcion de crear playlist

--- api_spotify.py
+++ api_spotify.py
@@ -13,19 +13,39 @@ def pedir_token():
     token = tk.prompt_for_user_token(*conf, scope=tk.scope.every)
     return token
 
-def mostrar_playlist(token) -> None:
+def mostrar_playlist(spotify) -> None:
     """
     Muestra las primeras 20 playlists que tenga el usuario.
-    Precondición: Recibe el token obtenido de la Api.
+    Precondición: Recibe una instancia de la clase Spotify creada a partir del token.
     """
-    contador: int = 0
-    spotify = tk.Spotify(token)
-    playlist = spotify.followed_playlists(limit=20).items
+    contador: int = int()
+    playlists = spotify.followed_playlists(limit=20).items
 
-    print('Listas de reproducción: ')
-    for track in playlist:
+    print('\nListas de reproducción: ')
+
+    for track in playlists:
         print(f'{contador + 1} - {track.name}')
         contador += 1
 
+def crear_playlist(spotify) -> None:
+    """
+    Crea una playlist vacía a partir de los datos que ingresa el usuario.
+    Precondicion: Recibe una instancia de la clase Spotify creada a partir del token.
+    """
+    user_id: str = spotify.current_user().id
+    nombre: str = input('Ingrese el nombre de la playlist: ')
+    entrada: str = input('¿Desea que la playlist sea publica? (Y/N): ')
+    
+    while entrada != 'Y' and entrada != 'N':
+        
+        print('Opción inválida.')
+        entrada: str = input('¿Desea que la playlist sea publica? (Y/N): ')
 
+    if entrada == 'Y':
+        publico: bool = True
+    elif entrada == 'N':
+        publico: bool = False
 
+    descripcion: str = input('Escriba una descripción: ')
+
+    spotify.playlist_create(user_id, nombre, publico, descripcion)
