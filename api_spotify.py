e023c8b2e0d17462525795600b5260055da75f33
Infernalizado <apanozzo@fi.uba.ar>
2022-06-18 21:22:14 -0300

    Documento la función y agrego un valor de retorno

--- api_spotify.py
+++ api_spotify.py
@@ -1,11 +1,19 @@
 import tekore as tk
 
-def pedir_token() -> None:
-    client_id = '6d3faa7cfb01460bacc1605a2f508e0d'
-    client_secret = '1e159178e8ca443498e3ec58f25fd792'
-    redirect_uri = 'https://example.com/callback'
-    conf = (client_id, client_secret, redirect_uri)
-    file = 'tekore.cfg'
+def pedir_token():
+    """
+    La funcion hará el request del token a la api a traves de Tekore.
+    Postcondiciones: Retornará el token obtenido a partir de los datos del usuario.
+    """
+    client_id: str = '6d3faa7cfb01460bacc1605a2f508e0d'
+    client_secret: str = '1e159178e8ca443498e3ec58f25fd792'
+    redirect_uri: str = 'https://example.com/callback'
+    conf: tuple = (client_id, client_secret, redirect_uri)
+    #file: str = 'tekore.cfg'
 
     token = tk.prompt_for_user_token(*conf, scope=tk.scope.every)
-    tk.config_to_file(file, conf + (token.refresh_token,))
+    #tk.config_to_file(file, conf + (token.refresh_token,))
+    return token
+
+
+
