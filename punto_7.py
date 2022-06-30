830bd1db027ba0e669c8e70a39e63ea5c8fa4a74
Manuel <mpolitis@fi.uba.ar>
2022-06-30 17:23:33 -0300

    resuelvo error punto 7 al ingresar playlists vacias

--- punto_7.py
+++ punto_7.py
@@ -3,103 +3,107 @@ import requests
 def punto7(letras):
     letras = letras
 
-    letras_corregidas = []
+    if len(letras) == 0:
+        print('\nPlaylist Vacia. Pruebe introducir otra.')
 
-    rankings_letras = []
+    else:
+        letras_corregidas = []
 
-    for letra in letras:
+        rankings_letras = []
 
-        # Elimino todos los corchetes y dejar renglones de las letras para poder identificar las palabras de las letras asi es posible hacer el ranking
+        for letra in letras:
 
-        letra = letra.replace('\n', " ")
+            # Elimino todos los corchetes y dejar renglones de las letras para poder identificar las palabras de las letras asi es posible hacer el ranking
 
-        lista_letra: list = []
+            letra = letra.replace('\n', " ")
 
-        palabra: list = []
+            lista_letra: list = []
 
-        parar_agregado = False
+            palabra: list = []
 
-        for i in letra:
+            parar_agregado = False
 
-            if i == "[" or i == "(":
-                parar_agregado = True
+            for i in letra:
 
-            if i != " " and parar_agregado == False and i != ',' and i != '.':
-                palabra.append(i)
+                if i == "[" or i == "(":
+                    parar_agregado = True
 
-            elif i == " " and parar_agregado == False:
-                lista_letra.append(("".join(palabra)).lower())
-                palabra = []
+                if i != " " and parar_agregado == False and i != ',' and i != '.':
+                    palabra.append(i)
 
-            if i == "]" or i == ")":
-                parar_agregado = False
+                elif i == " " and parar_agregado == False:
+                    lista_letra.append(("".join(palabra)).lower())
+                    palabra = []
+
+                if i == "]" or i == ")":
+                    parar_agregado = False
+
+                for i in lista_letra:
+                    if i == "" or i == "'" or i == "'":
+                        lista_letra.remove(i)
+
+            letras_corregidas.append(' '.join(lista_letra))
+
+            ranking: dict = {}
 
             for i in lista_letra:
-                if i == "" or i == "'" or i == "'":
-                    lista_letra.remove(i)
+                if i not in ranking.keys():
+                    ranking[i] = 1
 
-        letras_corregidas.append(' '.join(lista_letra))
+                elif i in ranking.keys():
+                    ranking[i] += 1
 
-        ranking: dict = {}
+            list_ranking = sorted(ranking.items(), key=lambda x: x[1], reverse=True)
 
-        for i in lista_letra:
-            if i not in ranking.keys():
-                ranking[i] = 1
+            while len(list_ranking) > 10:
+                list_ranking.pop()
 
-            elif i in ranking.keys():
-                ranking[i] += 1
+            ranking = dict(list_ranking)
 
-        list_ranking = sorted(ranking.items(), key=lambda x: x[1], reverse=True)
+            rankings_letras.append(ranking)
 
+        # Uno todos los rankings de las letras en uno solo
+        ranking_total = {}
+        for diccionario in rankings_letras:
+
+            for key, value in diccionario.items():
+                if key in ranking_total.keys():
+                    ranking_total[key] += value
+
+                else:
+                    ranking_total[key] = value
+
+        # Ordeno los valores del ranking total de mayor a menor
+
+        list_ranking = sorted(ranking_total.items(), key=lambda x: x[1], reverse=True)
+
+        # Limito el ranking a 10 valores nomas
         while len(list_ranking) > 10:
             list_ranking.pop()
 
-        ranking = dict(list_ranking)
+        ranking_total = dict(list_ranking)
 
-        rankings_letras.append(ranking)
+        print('Ranking de palabras:')
 
-    # Uno todos los rankings de las letras en uno solo
-    ranking_total = {}
-    for diccionario in rankings_letras:
+        contador = 1
+        for key, value in ranking_total.items():
+            print(f'Top {contador}: {key} con {value} repeticiones')
+            contador += 1
 
-        for key, value in diccionario.items():
-            if key in ranking_total.keys():
-                ranking_total[key] += value
+        # Creacion nube de palabras con todas las letras juntas
 
-            else:
-                ranking_total[key] = value
+        print("Creando nube de palabras...")
 
-    # Ordeno los valores del ranking total de mayor a menor
+        resp = requests.post('https://quickchart.io/wordcloud', json={
+            'format': 'png',
+            'width': 1000,
+            'height': 1000,
+            'fontScale': 15,
+            'scale': 'linear',
+            'text': (" ".join(letras_corregidas)),
+        })
 
-    list_ranking = sorted(ranking_total.items(), key=lambda x: x[1], reverse=True)
+        with open('newscloud.png', 'wb') as f:
+            f.write(resp.content)
 
-    # Limito el ranking a 10 valores nomas
-    while len(list_ranking) > 10:
-        list_ranking.pop()
-
-    ranking_total = dict(list_ranking)
-
-    print('Ranking de palabras:')
-
-    contador = 1
-    for key, value in ranking_total.items():
-        print(f'Top {contador}: {key} con {value} repeticiones')
-        contador += 1
-
-    # Creacion nube de palabras con todas las letras juntas
-
-    print("Creando nube de palabras...")
-
-    resp = requests.post('https://quickchart.io/wordcloud', json={
-        'format': 'png',
-        'width': 1000,
-        'height': 1000,
-        'fontScale': 15,
-        'scale': 'linear',
-        'text': (" ".join(letras_corregidas)),
-    })
-
-    with open('newscloud.png', 'wb') as f:
-        f.write(resp.content)
-
-    print("Nube de palabras creada con exito!")
+        print("Nube de palabras creada con exito!")
